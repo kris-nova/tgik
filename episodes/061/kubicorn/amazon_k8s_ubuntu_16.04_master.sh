91aab74d8b70c056a82911548b0c5886909bb7b1
Kris Nova <kris@nivenly.com>
2019-01-11 11:24:08 -0800

    meeps this works

--- episodes/061/kubicorn/amazon_k8s_ubuntu_16.04_master.sh
+++ episodes/061/kubicorn/amazon_k8s_ubuntu_16.04_master.sh
@@ -47,10 +47,8 @@ PORT=$(cat /etc/kubicorn/cluster.json | jq -r '.clusterAPI.spec.providerConfig' 
 HOSTNAME=$(hostname -f)
 
 cat << EOF  > "/etc/kubicorn/kubeadm-config.yaml"
-apiEndpoint:
-  advertiseAddress: ${PUBLICIP}
-  bindPort: ${PORT}
-apiVersion: kubeadm.k8s.io/v1alpha3
+apiVersion: kubeadm.k8s.io/v1beta1
+kind: InitConfiguration
 bootstrapTokens:
 - groups:
   - system:bootstrappers:kubeadm:default-node-token
@@ -59,47 +57,90 @@ bootstrapTokens:
   usages:
   - signing
   - authentication
-kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: ${PRIVATEIP}
+  bindPort: ${PORT}
 nodeRegistration:
   criSocket: /var/run/dockershim.sock
   kubeletExtraArgs:
     cloud-provider: aws
+    pod-infra-container-image: k8s.gcr.io/pause:3.1
   name: ${HOSTNAME}
   taints:
   - effect: NoSchedule
     key: node-role.kubernetes.io/master
 ---
-apiServerCertSANs:
-- ${PUBLICIP}
-- ${HOSTNAME}
-- ${PRIVATEIP}
-apiServerExtraArgs:
-  cloud-provider: aws
-  audit-dynamic-configuration: "true"
-  feature-gates: DynamicAuditing=true
-  runtime-config: auditregistration.k8s.io/v1alpha1=true
-apiVersion: kubeadm.k8s.io/v1alpha3
-auditPolicy:
-  logDir: /var/log/kubernetes/audit
-  logMaxAge: 2
-  path: ""
+apiVersion: kubeadm.k8s.io/v1beta1
+kind: ClusterConfiguration
+apiServer:
+  certSANs:
+  - ${PUBLICIP}
+  - ${HOSTNAME}
+  - ${PRIVATEIP}
+  extraArgs:
+    cloud-provider: aws
+    audit-dynamic-configuration: "true"
+    feature-gates: DynamicAuditing=true
+    runtime-config: auditregistration.k8s.io/v1alpha1=true
+  timeoutForControlPlane: 4m0s
 certificatesDir: /etc/kubernetes/pki
 clusterName: kubernetes
-controlPlaneEndpoint: ""
-controllerManagerExtraArgs:
-  cloud-provider: aws
+controlPlaneEndpoint: "${PUBLICIP}:${PORT}"
+controllerManager:
+  extraArgs:
+    cloud-provider: aws
+dns:
+  type: CoreDNS
+  imageTag: ${COREDNS_IMAGE_TAGS[0]}
 etcd:
   local:
     dataDir: /var/lib/etcd
-    image: ""
+    imageTag: ${ETCD_IMAGE_TAGS[0]}
 imageRepository: k8s.gcr.io
-kind: ClusterConfiguration
 kubernetesVersion: ${KUBERNETES_VERSION}
 networking:
   dnsDomain: cluster.local
-  podSubnet: ""
+  podSubnet: 192.168.0.0/16
   serviceSubnet: 10.96.0.0/12
-unifiedControlPlaneImage: ""
+scheduler: {}
+---
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+kind: KubeProxyConfiguration
+bindAddress: 0.0.0.0
+clientConnection:
+  acceptContentTypes: ""
+  burst: 10
+  contentType: application/vnd.kubernetes.protobuf
+  kubeconfig: /var/lib/kube-proxy/kubeconfig.conf
+  qps: 5
+clusterCIDR: 192.168.0.0/16
+configSyncPeriod: 15m0s
+conntrack:
+  max: null
+  maxPerCore: 32768
+  min: 131072
+  tcpCloseWaitTimeout: 1h0m0s
+  tcpEstablishedTimeout: 24h0m0s
+enableProfiling: false
+healthzBindAddress: 0.0.0.0:10256
+hostnameOverride: ""
+iptables:
+  masqueradeAll: false
+  masqueradeBit: 14
+  minSyncPeriod: 0s
+  syncPeriod: 30s
+ipvs:
+  excludeCIDRs: null
+  minSyncPeriod: 0s
+  scheduler: ""
+  syncPeriod: 30s
+metricsBindAddress: 127.0.0.1:10249
+mode: ""
+nodePortAddresses: null
+oomScoreAdj: -999
+portRange: ""
+resourceContainer: /kube-proxy
+udpIdleTimeout: 250ms
 EOF
 
 kubeadm reset
