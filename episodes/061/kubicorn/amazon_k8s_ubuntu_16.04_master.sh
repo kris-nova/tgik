f0cfd400105df474cd28acd76243e70c9ef26358
Kris Nova <kris@nivenly.com>
2019-01-11 10:32:38 -0800

    Another tweak

--- episodes/061/kubicorn/amazon_k8s_ubuntu_16.04_master.sh
+++ episodes/061/kubicorn/amazon_k8s_ubuntu_16.04_master.sh
@@ -5,7 +5,7 @@
 # ------------------------------------------------------------------------------------------------------------------------
 
 # Specify the Kubernetes version to use.
-KUBERNETES_VERSION="v1.13.2"
+KUBERNETES_VERSION="1.13.2"
 KUBERNETES_CNI="0.6.0"
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
